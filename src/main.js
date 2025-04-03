9734ca2d8cc3270bedaf20f90380fe10e00ce7f9
SoftCore <soft0core1@gmail.com>
2025-04-03 21:40:54 +0100

    complete

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ let mobileNav = document.querySelector(".mobile-nav")
 let closeButton = document.querySelector(".close-div")
 
 burgerButton.addEventListener("click", (e) => {
+    e.stopPropagation();
     mobileNav.style.display = "flex"
 })
 
 closeButton.addEventListener("click", (e) => {
+    e.stopPropagation();
     mobileNav.style.display = "none"
 })
 
-
+window.addEventListener("click", (e) => {
+    if (!mobileNav.contains(e.target)) mobileNav.style.display = "none";
+})
 
